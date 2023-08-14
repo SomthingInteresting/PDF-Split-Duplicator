ed4eab008664050fdd743b0c442e99d5acaf43f5
Quddus Rahman <qrahman115@gmail.com>
2023-08-14 12:51:50 +0100

    added folder group function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
-from tkinter import filedialog, messagebox
-from pdf_operations import split_pdf, copy_file_to_destinations
+from tkinter import filedialog, messagebox, simpledialog
+from pdf_operations import split_pdf, copy_file_to_destinations, save_folder_group, load_folder_groups, load_selected_group
 
 def main():
     root = tk.Tk()
@@ -21,37 +21,54 @@ def main():
     folders_listbox.pack()
 
     # Button to add folders
-    button_add = tk.Button(root, text="Add Folder", command=lambda: add_folder_to_list(payslip_var.get(), folders_listbox))
+    button_add = tk.Button(root, text="Add Folder", command=lambda: copy_file_to_destinations(payslip_var.get(), folders_listbox))
     button_add.pack()
 
-    # Button to remove folders
+    # Button to remove selected folder
     def remove_selected_folder():
-        selected = folders_listbox.curselection()  # Gets the index of the selected item
-        if selected:
-            folders_listbox.delete(selected)
+        if folders_listbox.curselection():
+            folders_listbox.delete(folders_listbox.curselection()[0])
 
     button_remove = tk.Button(root, text="Remove Folder", command=remove_selected_folder)
     button_remove.pack()
 
+    # Listbox to show saved groups
+    groups_listbox = tk.Listbox(root)
+    groups_listbox.pack()
+    for group in load_folder_groups():
+        groups_listbox.insert(tk.END, group)
+
+    # Button to save current folder group
+    def save_current_group():
+        group_name = simpledialog.askstring("Input", "Enter name for the folder group:")
+        if group_name:
+            folders = folders_listbox.get(0, tk.END)
+            save_folder_group(group_name, folders)
+            groups_listbox.insert(tk.END, group_name)
+
+    button_save_group = tk.Button(root, text="Save Group", command=save_current_group)
+    button_save_group.pack()
+
+    # Button to load selected group to main folder listbox
+    def load_group():
+        selected_group = groups_listbox.get(groups_listbox.curselection())
+        if selected_group:
+            folders = load_selected_group(selected_group)
+            folders_listbox.delete(0, tk.END)  # Clear current list
+            for folder in folders:
+                folders_listbox.insert(tk.END, folder)
+
+    button_load_group = tk.Button(root, text="Load Group", command=load_group)
+    button_load_group.pack()
+
     # Button to perform the copy action
-    button_copy = tk.Button(root, text="Copy to Selected Folders", command=lambda: copy_files_and_notify(payslip_var.get(), folders_listbox.get(0, tk.END)))
+    button_copy = tk.Button(root, text="Copy to Selected Folders", command=lambda: [copy_file_to_destinations(payslip_var.get(), folder) for folder in folders_listbox.get(0, tk.END)])
     button_copy.pack()
-
-    def copy_files_and_notify(payslip, folders):
-        success = copy_file_to_destinations(payslip, folders)
-        if success:
-            messagebox.showinfo("Success", "Files have been successfully copied!")
-        else:
-            messagebox.showerror("Error", "An error occurred while copying files.")
-
-    def add_folder_to_list(payslip, folders_listbox):
-        folder = filedialog.askdirectory(title="Select Folder")
-        if folder:
-            folders_listbox.insert(tk.END, folder)
 
     root.lift()
     root.attributes('-topmost', True)
     root.after_idle(root.attributes, '-topmost', False)
+
     root.mainloop()
 
 if __name__ == "__main__":
--- src/pdf_operations.py
+++ src/pdf_operations.py
@@ -1,6 +1,8 @@
 import PyPDF2
+import json
+import os
 import tkinter as tk
-from shutil import copy2  # Use copy2 to also copy file metadata
+from shutil import copy2
 from tkinter import filedialog
 
 def split_pdf(input_pdf):
@@ -21,11 +23,32 @@ def split_pdf(input_pdf):
 
     return payslips
 
-def copy_file_to_destinations(file_name, destinations):
-    try:
-        for dest in destinations:
-            copy2(file_name, dest)
-        return True
-    except Exception as e:
-        print(e)  # Log the exception for debugging purposes
-        return False
+def copy_file_to_destinations(payslip, folders_listbox):
+    folder = filedialog.askdirectory(title="Select Folder")
+    if folder:
+        folders_listbox.insert(tk.END, folder)
+
+FOLDER_GROUPS_FILE = "folder_groups.json"
+
+def save_folder_group(group_name, folders):
+    data = {}
+    if os.path.exists(FOLDER_GROUPS_FILE):
+        with open(FOLDER_GROUPS_FILE, "r") as f:
+            data = json.load(f)
+    data[group_name] = folders
+    with open(FOLDER_GROUPS_FILE, "w") as f:
+        json.dump(data, f)
+
+def load_folder_groups():
+    if os.path.exists(FOLDER_GROUPS_FILE):
+        with open(FOLDER_GROUPS_FILE, "r") as f:
+            data = json.load(f)
+        return list(data.keys())
+    return []
+
+def load_selected_group(group_name):
+    if os.path.exists(FOLDER_GROUPS_FILE):
+        with open(FOLDER_GROUPS_FILE, "r") as f:
+            data = json.load(f)
+        return data.get(group_name, [])
+    return []
